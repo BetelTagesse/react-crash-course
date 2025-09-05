c8a025bb0880b90d2e75f20409192ecff591b9ac
BetelTagesse <BetelTagesse367@gmail.com>
2025-09-05 22:46:17 +0300

    modal done

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 const PostsList = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
 
   const changeBodyHandler = (event) => {
     setEnteredBody(event.target.value);
@@ -17,17 +22,20 @@ const PostsList = () => {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onChangeAuthor={changeAuthorHandler}
-          onChangeBody={changeBodyHandler}
-        />
-        <ul className={classes.posts}>
-          <Post author={enteredAuthor} body={enteredBody} />
-          <Post author="bethany" body="this is beth" />
-          <Post author="candlaria" body="this is candy" />
-        </ul>
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onChangeAuthor={changeAuthorHandler}
+            onChangeBody={changeBodyHandler}
+          />
+        </Modal>
+      )}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="bethany" body="this is beth" />
+        <Post author="candlaria" body="this is candy" />
+      </ul>
     </>
   );
 };
